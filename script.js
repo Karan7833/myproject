ab403b19c9742410492502ce4c70bddcee13ceeb
Karan7833 <karanrathour1811@gmail.com>
2025-06-23 08:49:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const navLinks = document.querySelectorAll(".nav-links a");
+document.getElementById("contactForm").addEventListener("submit", async function (e) {
+  e.preventDefault();
 
-  navLinks.forEach(link => {
-    link.addEventListener("click", (e) => {
-      e.preventDefault();
-      const targetId = link.getAttribute("href");
-      document.querySelector(targetId).scrollIntoView({ behavior: "smooth" });
+ console.log("Form submitted");
+  const data = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value
+  };
+
+  try {
+    const res = await fetch("http://localhost:5000/contact", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
     });
-  });
+
+    const result = await res.json();
+    alert(result.message);
+  } catch (err) {
+    alert("Something went wrong. Try again later.");
+  }
 });
+
